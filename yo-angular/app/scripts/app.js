fa154057a3cf818360ae1665add40bb66b5501fb
Rodrigo Lemos <rodrigolemosoliveira@gmail.com>
2015-05-21 16:18:17 -0300

    Material de apoio para treinamento front-end

---
+++ yo-angular/app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name exampleApp
+ * @description
+ * # exampleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('exampleApp', [
+    'ngAnimate',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
